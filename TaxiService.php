1fe7b91488348d3774e7e9670442f13e6396864b
Tasdiq-Dewan <37335919+Tasdiq-Dewan@users.noreply.github.com>
2018-03-23 16:00:20 +0000

    Add files via upload

---
+++ TaxiService.php
@@ -0,0 +1,27 @@
+<?php
+
+class TaxiService
+{
+  private $pickupLocation;
+  //settype($pickupLocation, "string");
+  private $destination;
+  //settype($destination, "string");
+  private $numberOfPassengers;
+  //settype($numberOfPassengers, "int");
+  private $price;
+  //settype($price, "float");
+
+  function __construct($pickup, $dest, $number, $_price){
+    $this->pickupLocation = $pickup;
+    $this->destination = $dest;
+    $this->numberOfPassengers = $number;
+    $this->price = $_price;
+  }
+
+  function getTaxiInfo(){
+    $allInfo = array($this->pickupLocation, $this->destination, $this->numberOfPassengers, $this->price);
+    return $allInfo;
+  }
+}
+
+?>
